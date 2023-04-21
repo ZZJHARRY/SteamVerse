66146aafe3b4cfa10afa96e190ec31445fec5f71
gcheng9430 <59846118+gcheng9430@users.noreply.github.com>
2023-04-21 15:35:32 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ app.get('/search_songs', routes.search_songs);
 app.get('/recommendation/:type_of_recommendation',routes.recommendation);
 app.get('/games/:type_of_games',routes.games);
 app.get('/app/:app_id', routes.game);
+app.get('/system/:type_of_system', routes.game_system);
 app.get('/system/:app_id', routes.system);
 
 // app.get('/filtering',routes.search_filter);
