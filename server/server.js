dd0977d655dfd76150b76eac417587b0290d5071
zzjharry-upenn <112655343+zzjharry-upenn@users.noreply.github.com>
2023-04-21 14:46:34 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.get('/search_songs', routes.search_songs);
 
 
 app.get('/recommendation/:type_of_recommendation',routes.recommendation);
-app.get('/recommendation/:type_of_games',routes.games);
+app.get('/games/:type_of_games',routes.games);
 app.get('/app/:app_id', routes.game);
 app.get('/system/:app_id', routes.system);
 
