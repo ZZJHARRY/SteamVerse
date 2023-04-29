27cbf9c366eb02d582d0b0e39b64d6be532d32b8
zzjharry-upenn <112655343+zzjharry-upenn@users.noreply.github.com>
2023-04-29 15:45:36 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import { indigo, amber } from '@mui/material/colors'
-import { createTheme } from "@mui/material/styles";
+import { indigo, ambe, blue, orange } from '@mui/material/colors'
+import { createTheme, styled } from "@mui/material/styles";
 
-import NavBar from './components/NavBar';
+// import NavBar from './components/NavBar';
+import { NavLink } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import RecommendationPage from './pages/RecommendationPage'
 import GamesPage from './pages/GamesPage'
@@ -11,26 +12,201 @@ import FilteringPage from './pages/FilteringPage'
 import SystemsPage from "./pages/SystemsPage";
 import UsersPage from "./pages/UsersPage";
 
+import * as React from 'react';
+// import { styled, ThemeProvider } from '@mui/material/styles';
+// import CssBaseline from '@mui/material/CssBaseline';
+import MuiDrawer from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Container from '@mui/material/Container';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { mainListItems, secondaryListItems } from './listItems';
+
 // createTheme enables you to customize the look and feel of your app past the default
 // in this case, we only change the color scheme
-export const theme = createTheme({
+// export const theme = createTheme({
+//   palette: {
+//     primary: indigo,
+//     secondary: amber,
+//   },
+// });
+
+const NavText = ({ href, text, isMain }) => {
+  return (
+    <Typography
+      variant={isMain ? 'h5' : 'h7'}
+      noWrap
+      style={{
+        marginRight: '30px',
+        fontFamily: 'monospace',
+        fontWeight: 700,
+        letterSpacing: '.3rem',
+      }}
+    >
+      <NavLink
+        to={href}
+        style={{
+          color: 'inherit',
+          textDecoration: 'none',
+        }}
+      >
+        {text}
+      </NavLink>
+    </Typography>
+  )
+}
+
+const theme = createTheme({
   palette: {
-    primary: indigo,
-    secondary: amber,
+    primary: {
+      main: orange[300],
+    },
+    secondary: {
+      main: blue[500],
+    },
   },
 });
+
+const drawerWidth = 300;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    '& .MuiDrawer-paper': {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      boxSizing: 'border-box',
+      ...(!open && {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+          width: theme.spacing(9),
+        },
+      }),
+    },
+  }),
+);
+
+// const mdTheme = createTheme();
+
 
 // App is the root component of our application and as children contain all our pages
 // We use React Router's BrowserRouter and Routes components to define the pages for
 // our application, with each Route component representing a page and the common
 // NavBar component allowing us to navigate between pages (with hyperlinks)
 export default function App() {
+    const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter>
-        <NavBar />
-        <Routes>
+        {/* <NavBar /> */}
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="absolute" open={open}>
+          <Toolbar
+            sx={{
+              pr: '24px', // keep right padding when drawer closed
+            }}
+          >
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: '36px',
+                ...(open && { display: 'none' }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              <NavText href='/' text='STEAMVERSE' isMain />
+            </Typography>
+            {/* <IconButton color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton> */}
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+              px: [1],
+            }}
+          >
+            <IconButton onClick={toggleDrawer}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List component="nav">
+            {mainListItems}
+            {/* <Divider sx={{ my: 1 }} />
+            {secondaryListItems} */}
+          </List>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/recommendation" element={<RecommendationPage />} />
           <Route path="/games" element={<GamesPage />} />
@@ -39,6 +215,10 @@ export default function App() {
           <Route path="/users" element={<UsersPage />} />
 
         </Routes>
+    
+          </Container>
+        </Box>
+      </Box>
       </BrowserRouter>
     </ThemeProvider>
   );
