f4797daac249e8731a6e70e081aedce1866b3854
zzjharry-upenn <112655343+zzjharry-upenn@users.noreply.github.com>
2023-05-02 15:25:56 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,17 +10,6 @@ app.use(cors({
 
 // We use express to define our various API endpoints and
 // provide their handlers that we implemented in routes.js
-app.get('/author/:type', routes.author);
-app.get('/random', routes.random);
-app.get('/song/:song_id', routes.song);
-app.get('/album/:album_id', routes.album);
-app.get('/albums', routes.albums);
-app.get('/album_songs/:album_id', routes.album_songs);
-app.get('/top_songs', routes.top_songs);
-app.get('/top_albums', routes.top_albums);
-app.get('/search_songs', routes.search_songs);
-
-
 app.get('/recommendation/:type_of_recommendation',routes.recommendation);
 app.get('/games/:type_of_games',routes.games);
 app.get('/app/:app_id', routes.game);
